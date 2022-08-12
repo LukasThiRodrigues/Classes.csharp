e3d81f0738af32c4449d13ba72c6b0e6c6b6e2fd
LukasThiRodrigues <lukasthi7@gmail.com>
2022-08-11 21:35:42 -0300

    huh

---
+++ Códigos_csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// cria dois objetos do tipo cidade
+Cidade cid1 = new Cidade();
+Cidade cid2 = new Cidade();
+
+// coloca informações sobre cada cidade nos respectivos objetos
+cid1.nome = "Joinville";
+cid1.Habitantes = 600000;
+
+cid2.nome = "Curitiba";
+cid2.Habitantes = 1200000;
+
+// faz cada objeto mostrar seus dados
+Console.WriteLine( cid1.mostrarDados() );
+Console.WriteLine( cid2.mostrarDados() );
