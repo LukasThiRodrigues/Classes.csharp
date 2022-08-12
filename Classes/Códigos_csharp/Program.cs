9ac5f639691a0d6b0c6a0de93f89ec584343b494
LukasThiRodrigues <107968063+LukasThiRodrigues@users.noreply.github.com>
2022-08-11 22:15:55 -0300

    Update Program.cs

--- Classes/Códigos_csharp/Program.cs
+++ Classes/Códigos_csharp/Program.cs
@@ -12,3 +12,18 @@ cid2.Habitantes = 1200000;
 // faz cada objeto mostrar seus dados
 Console.WriteLine( cid1.mostrarDados() );
 Console.WriteLine( cid2.mostrarDados() );
+
+correntista crr1 = new correntista();
+correntista crr2 = new correntista();
+
+crr1.cpf = "141.024.549-77";
+crr1.nome = "Lukas Rodrigues";
+crr1.telefone = "+55 (47) 99613-5693";
+
+crr2.cpf = "091.245.678-90";
+crr2.nome = "Roger klaus";
+crr2.telefone = "+55 (47) 99999-5490";
+
+Console.Clear();
+Console.WriteLine( crr1.mostrarDados() );
+Console.WriteLine( crr2.mostrarDados() );
