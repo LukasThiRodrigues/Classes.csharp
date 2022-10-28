533e661493606f385fdb5aa0eba6e162ebaee9df
LukasThiRodrigues <lukasthi7@gmail.com>
2022-10-28 16:30:08 -0300

    trabalho

---
+++ CRUD/Program.cs
@@ -0,0 +1,25 @@
+﻿Tela tela = new Tela();
+ControleConta contas = new ControleConta(tela);
+
+string opcao;
+List<string> menuPrincipal = new List<string>();
+
+menuPrincipal.Add("1 - Lista        ");
+menuPrincipal.Add("2 - Pesquisar    ");
+menuPrincipal.Add("3 - Favoritos    ");
+menuPrincipal.Add("0 - Sair         ");
+
+tela.configurarTela();
+
+while(true){
+    tela.montarTelaSistema();
+    opcao = tela.mostrarMenu(menuPrincipal,3,3);
+
+    if(opcao == "0") break;
+    if(opcao == "2") contas.executarCRUD();
+}
+
+//tela.montarMoldura(5,5,40,10);
+//tela.montarMoldura(10,7,50,20);
+
+Console.Clear();
